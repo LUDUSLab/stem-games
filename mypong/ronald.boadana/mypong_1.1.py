afa6e079ebf28e46e46e23f40f488a564b25559f
Ronald Boadana <rcb.eng20@uea.edu.br>
2021-02-06 16:45:03 -0400

    A.I. is already working

--- mypong/ronald.boadana/mypong_1.1.py
+++ mypong/ronald.boadana/mypong_1.1.py
@@ -1,8 +1,9 @@
-# By Jucimar Jr 2019 and adapted by Ronald Boadana
-# pong em turtle python https://docs.python.org/3.3/library/turtle.html
+# By Jucimar Jr 2019
+# Pong in turtle python https://docs.python.org/3.3/library/turtle.html
 # Based on http://christianthompson.com/node/51
 # Font: Press Start 2P https://www.fontspace.com/code88man38/press-start-2p
 # Score sound https://freesound.org/people/Kodack/sounds/258020/
+# Powered by Ronald Boadana
 
 # import required library
 import turtle
@@ -17,7 +18,7 @@ screen.bgcolor("black")
 screen.setup(width=800, height=600)
 screen.tracer(0)
 
-# how to draw the left paddle
+# draw the left paddle
 paddle_1 = turtle.Turtle()
 paddle_1.speed(0)
 paddle_1.shape("square")
@@ -26,7 +27,7 @@ paddle_1.shapesize(stretch_wid=5, stretch_len=2)
 paddle_1.penup()
 paddle_1.goto(-360, 0)
 
-# how to draw the 'A.I.' paddle
+# draw the 'A.I.' paddle
 paddle_ai = turtle.Turtle()
 paddle_ai.speed(0)
 paddle_ai.shape("square")
@@ -35,7 +36,7 @@ paddle_ai.shapesize(stretch_wid=5, stretch_len=2)
 paddle_ai.penup()
 paddle_ai.goto(360, 0)
 
-# how to draw the ball
+# draw the ball
 ball = turtle.Turtle()
 ball.speed(1)
 ball.shape("square")
@@ -55,7 +56,7 @@ hud.shape("square")
 hud.color("white")
 hud.penup()
 hud.goto(0, 260)
-hud.write("0 : 0", align="center", font=("PressStart2P ", 24, "normal"))
+hud.write("0 x 0", align="center", font=("Press Start 2P", 24, "normal"))
 hud.hideturtle()
 
 
@@ -79,6 +80,26 @@ def paddle_1_down():
     paddle_1.sety(y)
 
 
+# 'A.I.' paddle going up
+def paddle_ai_up():
+    y = paddle_ai.ycor()
+    if y < 250:
+        y += 50
+    else:
+        y = 250
+    paddle_ai.sety(y)
+
+
+# 'A.I.' paddle going down
+def paddle_ai_down():
+    y = paddle_ai.ycor()
+    if y > -250:
+        y += -50
+    else:
+        y = -250
+    paddle_ai.sety(y)
+
+
 # ball randomness
 def ale_ball():
     randomness = (random.randrange(-20, 22)) / 20
@@ -86,7 +107,6 @@ def ale_ball():
 
 
 # mapping the keyboard
-# can move the right paddle with two options
 screen.listen()
 screen.onkeypress(paddle_1_up, 'w')
 screen.onkeypress(paddle_1_down, 's')
@@ -96,17 +116,12 @@ while True:
     # ball movement
     ball.setx(ball.xcor() + ball.dx)
     ball.sety(ball.ycor() + ball.dy)
-    # "A.I." paddle
-    paddle_ai.sety(ball.ycor())
-    # random loss of concentration of 'AI' still in developing
-    paddle_ai_fail_pos = 1.0
-    paddle_ai_fail_neg = -1.0
-    chance_fail_ai = random.randint(-1, 1)
-    print(chance_fail_ai)
-    if chance_fail_ai == paddle_ai_fail_pos:
-        paddle_ai.sety(ball.ycor() + 30)
-    elif chance_fail_ai == paddle_ai_fail_neg:
-        paddle_ai.sety(ball.ycor() - 30)
+
+    # any random loss of concentration of 'A.I.'
+    if ball.ycor() > paddle_ai.ycor() + 10 and random.randrange(21) == 10:
+        paddle_ai_up()
+    if ball.ycor() < paddle_ai.ycor() - 10 and random.randrange(21) == 10:
+        paddle_ai_down()
 
     # ball collision with the borders
     # upper
@@ -125,8 +140,7 @@ while True:
     if ball.xcor() < -390:
         score_2 += 1
         hud.clear()
-        hud.write("{} : {}".format(score_1, score_2), align="center", font=("Press"
-                                                                            "Start2P", 24, "normal"))
+        hud.write("{} x {}".format(score_1, score_2), align="center", font=("Press Start 2P", 24, "normal"))
         winsound.PlaySound("C:/Users/bh_ro/Documents/STEM/stem-games/mypong2/assets/258020__kodack__arcade-bleep"
                            "-sound.wav", winsound.SND_ASYNC)
         # os.system("afplay 258020_Kodak_arcade-bleep-sound.wav&")
@@ -138,8 +152,7 @@ while True:
     if ball.xcor() > 390:
         score_1 += 1
         hud.clear()
-        hud.write("{} : {}".format(score_1, score_2), align="center", font=("Press"
-                                                                            "-Start2P", 24, "normal"))
+        hud.write("{} x {}".format(score_1, score_2), align="center", font=("Press Start 2P", 24, "normal"))
         winsound.PlaySound("C:/Users/bh_ro/Documents/STEM/stem-games/mypong2/assets/258020__kodack__arcade-bleep"
                            "-sound.wav", winsound.SND_ASYNC)
         # os.system("afplay 258020_kodack_arcade-bleep-sound.wav&")
@@ -148,14 +161,14 @@ while True:
         ball.dy = ale_ball()  # ball randomness
 
     # ball collision with the left paddle
-    if ball.xcor() == -350 and paddle_1.ycor() + 50 > ball.ycor() > paddle_1.ycor() - 50:
+    if ball.xcor() == -340 and paddle_1.ycor() + 50 > ball.ycor() > paddle_1.ycor() - 50:
         ball.dx *= -1
         ball.dy = ale_ball()  # any randomness for ball angle
         winsound.PlaySound("C:/Users/bh_ro/Documents/STEM/stem-games/mypong2/assets/bounce.wav", winsound.SND_ASYNC)
         # os.system("afplay bounce.wav&")
 
     # ball collision with the right paddle
-    if ball.xcor() == 350 and paddle_ai.ycor() + 50 > ball.ycor() > paddle_ai.ycor() - 50:
+    if ball.xcor() == 340 and paddle_ai.ycor() + 50 > ball.ycor() > paddle_ai.ycor() - 50:
         ball.dx *= -1
         ball.dy = ale_ball()  # any randomness for ball angle
         winsound.PlaySound("C:/Users/bh_ro/Documents/STEM/stem-games/mypong2/assets/bounce.wav", winsound.SND_ASYNC)
