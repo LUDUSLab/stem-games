dcca765a0b3126187b3817ad3e148aada8e0c21c
Josué Costa dos Santos Alves <jcsa.eng17@uea.edu.br>
2021-02-07 19:21:25 -0400

    inserindo IA

--- mypong/josue.alves/mypong.py
+++ mypong/josue.alves/mypong.py
@@ -1,22 +1,27 @@
-# Jucimar Jr 2019
-# Version by Matheus Nielsen 2021
-# pong using turtle python https://docs.python.org/3.3/library/turtle.html
-# based on http://christianthompson.com/node/51
-# Press Start 2P font https://www.fontspace.com/codeman38/press-start-2p
-# score sound effect https://freesound.org/people/Kodack/sounds/258020/
+# Josué Alves 2021
+# pong em turtle python https://docs.python.org/3.3/library/turtle.html
+# baseado em http://christianthompson.com/node/51
+# fonte Press Start 2P https://www.fontspace.com/codeman38/press-start-2p
+# som pontuação https://freesound.org/people/Kodack/sounds/258020/
 
 import turtle
 import winsound
 import random
 
-# draw background
+aux = [-1, 1]
+
+# sound path
+bounce = 'C:/Users/Josué/Documents/STEM/stem-games/mypong/assets/bounce.wav'
+bleep = 'C:/Users/Josué/Documents/STEM/stem-games/mypong/assets/258020__kodack__arcade-bleep-sound.wav'
+
+# draw screen
 screen = turtle.Screen()
 screen.title("My Pong")
 screen.bgcolor("black")
 screen.setup(width=800, height=600)
 screen.tracer(0)
 
-# draw paddle 1
+# draw racquet 1
 paddle_1 = turtle.Turtle()
 paddle_1.speed(0)
 paddle_1.shape("square")
@@ -25,7 +30,7 @@ paddle_1.shapesize(stretch_wid=5, stretch_len=1)
 paddle_1.penup()
 paddle_1.goto(-350, 0)
 
-# draw paddle 2
+# draw racquet 2
 paddle_2 = turtle.Turtle()
 paddle_2.speed(0)
 paddle_2.shape("square")
@@ -41,14 +46,14 @@ ball.shape("square")
 ball.color("white")
 ball.penup()
 ball.goto(0, 0)
-ball.dx = 1
-ball.dy = 0
+ball.dx = 0.4
+ball.dy = 0.4
 
 # score
 score_1 = 0
 score_2 = 0
 
-# score's hud
+# score display
 hud = turtle.Turtle()
 hud.speed(0)
 hud.shape("square")
@@ -61,6 +66,7 @@ hud.write("0 : 0", align="center", font=("Press Start 2P", 24, "normal"))
 
 def paddle_1_up():
     y = paddle_1.ycor()
+
     if y < 250:
         y += 30
     else:
@@ -71,7 +77,7 @@ def paddle_1_up():
 def paddle_1_down():
     y = paddle_1.ycor()
     if y > -250:
-        y += -30
+        y -= 30
     else:
         y = -250
     paddle_1.sety(y)
@@ -80,7 +86,7 @@ def paddle_1_down():
 def paddle_2_up():
     y = paddle_2.ycor()
     if y < 250:
-        y += 30
+        y += 5
     else:
         y = 250
     paddle_2.sety(y)
@@ -89,36 +95,13 @@ def paddle_2_up():
 def paddle_2_down():
     y = paddle_2.ycor()
     if y > -250:
-        y += -30
+        y -= 5
     else:
         y = -250
     paddle_2.sety(y)
 
 
-def wall_collision():
-    winsound.PlaySound('bounce.wav', winsound.SND_FILENAME | winsound.SND_ASYNC | winsound.SND_NOWAIT)
-    ball.dy *= -1
-
-
-def paddle_collision():
-    random_position = random.randrange(-7, 8)
-    ball.dy = random_position / 10
-    ball.dx *= -1
-    winsound.PlaySound('bounce.wav', winsound.SND_FILENAME | winsound.SND_ASYNC | winsound.SND_NOWAIT)
-
-
-def scoring():
-    hud.clear()
-    hud.write("{} : {}".format(score_1, score_2), align="center", font=("Press Start 2P", 24, "normal"))
-    winsound.PlaySound('258020__kodack__arcade-bleep-sound.wav', winsound.SND_FILENAME)
-    ball.goto(0, 0)
-    ball.dx = -0.5
-    ball.dy = 0
-    paddle_1.sety(0)
-    paddle_2.sety(0)
-
-
-# key mapping
+# mapping keys
 screen.listen()
 screen.onkeypress(paddle_1_up, "w")
 screen.onkeypress(paddle_1_down, "s")
@@ -132,24 +115,60 @@ while True:
     ball.setx(ball.xcor() + ball.dx)
     ball.sety(ball.ycor() + ball.dy)
 
-    # upper and lower wall collision
-    if ball.ycor() > 290 or ball.ycor() < -290:
-        wall_collision()
+    # collision with upper wall
+    if ball.ycor() > 290:
+        winsound.PlaySound(bounce, winsound.SND_ASYNC)
+        ball.sety(290)
+        ball.dy *= -1
 
-    # left wall collision
+    # collision with bottom wall
+    if ball.ycor() < -280:
+        winsound.PlaySound(bounce, winsound.SND_ASYNC)
+        ball.sety(-280)
+        ball.dy *= -1
+
+    # collision with left wall
     if ball.xcor() < -390:
         score_2 += 1
-        scoring()
-    
-    # right wall collision
+        hud.clear()
+        hud.write("{} : {}".format(score_1, score_2), align="center", font=("Press Start 2P", 24, "normal"))
+        winsound.PlaySound(bleep, winsound.SND_ASYNC)
+        ball.clear()
+        ball.goto(0, 0)
+        ball.dx *= random.choice(aux)
+        ball.dy *= random.choice(aux)
+
+    # collision with right wall
     if ball.xcor() > 390:
         score_1 += 1
-        scoring()
+        hud.clear()
+        hud.write("{} : {}".format(score_1, score_2), align="center", font=("Press Start 2P", 24, "normal"))
+        winsound.PlaySound(bleep, winsound.SND_ASYNC)
+        ball.clear()
+        ball.goto(0, 0)
+        ball.dx *= random.choice(aux)
+        ball.dy *= random.choice(aux)
 
-    # paddle 1 collision
-    if ball.xcor() == -350 and paddle_1.ycor() + 50 > ball.ycor() > paddle_1.ycor() - 50:
-        paddle_collision()
-    
-    # paddle 2 collision
-    if ball.xcor() == 350 and paddle_2.ycor() + 50 > ball.ycor() > paddle_2.ycor() - 50:
-        paddle_collision()
+    # collision with racquet 1
+    if (-330 > ball.xcor() > -340) \
+            and (ball.ycor() < paddle_1.ycor() + 50) \
+            and (ball.ycor() > paddle_1.ycor() - 50):
+        ball.setx(-330)
+        ball.dx *= -1
+        ball.dy *= random.choice(aux)
+        winsound.PlaySound(bounce, winsound.SND_ASYNC)
+
+    # collision with racquet 2
+    if (330 < ball.xcor() < 340) \
+            and (ball.ycor() < paddle_2.ycor() + 50) \
+            and (ball.ycor() > paddle_2.ycor() - 50):
+        ball.setx(330)
+        ball.dx *= -1
+        ball.dy *= random.choice(aux)
+        winsound.PlaySound(bounce, winsound.SND_ASYNC)
+
+# AI player
+    if paddle_2.ycor() < ball.ycor() - 60:
+        paddle_2_up()
+    elif paddle_2.ycor() > ball.ycor() + 60:
+        paddle_2_down()
