deeeb0c90cb340e898c079a284266a7c9b8f18c7
Ronald Boadana <rcb.eng20@uea.edu.br>
2021-02-10 11:50:28 -0400

    i debugged somethings on code and i added the randomness on the ball

--- mypong2/ronald.boadana/mypongpygame.py
+++ mypong2/ronald.boadana/mypongpygame.py
@@ -1,52 +1,51 @@
 import pygame
+import random  # for ball randomness
 
 pygame.init()
 
 COLOR_BLACK = (0, 0, 0)
 COLOR_WHITE = (255, 255, 255)
 
-SCORE_MAX = 2
+SCORE_MAX = 10
 
 size = (1280, 720)
 screen = pygame.display.set_mode(size)
-pygame.display.set_caption("MyPong - PyGame Edition - 2021.01.30")
+pygame.display.set_caption("MyPong2.0 Alpha 0.1 - PyGame Edition - 2021.02.10")
 
 # score text
-score_font = pygame.font.Font('assets/PressStart2P.ttf', 44)
+score_font = pygame.font.Font('C:/Users/bh_ro/Documents/STEM/stem-games/mypong2/assets/PressStart2P.ttf', 44)
 score_text = score_font.render('00 x 00', True, COLOR_WHITE, COLOR_BLACK)
 score_text_rect = score_text.get_rect()
 score_text_rect.center = (680, 50)
 
 # victory text
-victory_font = pygame.font.Font('assets/PressStart2P.ttf', 100)
-victory_text = victory_font .render('VICTORY', True, COLOR_WHITE, COLOR_BLACK)
+victory_font = pygame.font.Font('C:/Users/bh_ro/Documents/STEM/stem-games/mypong2/assets/PressStart2P.ttf', 100)
+victory_text = victory_font.render('VICTORY', True, COLOR_WHITE, COLOR_BLACK)
 victory_text_rect = score_text.get_rect()
 victory_text_rect.center = (450, 350)
 
 # sound effects
-bounce_sound_effect = pygame.mixer.Sound('assets/bounce.wav')
-scoring_sound_effect = pygame.mixer.Sound('assets/258020__kodack__arcade-bleep-sound.wav')
+bounce_sound_effect = pygame.mixer.Sound('C:/Users/bh_ro/Documents/STEM/stem-games/mypong2/assets/bounce.wav')
+scoring_sound_effect = pygame.mixer.Sound('C:/Users/bh_ro/Documents/STEM/stem-games/mypong2/assets'
+                                          '/258020__kodack__arcade-bleep-sound.wav')
 
 # player 1
-player_1 = pygame.image.load("assets/player.png")
+player_1 = pygame.image.load('C:/Users/bh_ro/Documents/STEM/stem-games/mypong2/assets/ronald.boadana_paddle.png')
 player_1_y = 300
 player_1_move_up = False
 player_1_move_down = False
 
 # player 2 - robot
-player_2 = pygame.image.load("assets/player.png")
+player_2 = pygame.image.load('C:/Users/bh_ro/Documents/STEM/stem-games/mypong2/assets/ronald.boadana_paddle.png')
 player_2_y = 300
 
 # ball
-ball = pygame.image.load("assets/ball.png")
-ball_x = 640
-ball_y = 360
-ball_dx = 5
-ball_dy = 5
+ball = pygame.image.load('C:/Users/bh_ro/Documents/STEM/stem-games/mypong2/assets/ronald.boadana_ball.png')
+ball_x, ball_y = 640, 360
+ball_speed_x = ball_speed_y = 7
 
 # score
-score_1 = 0
-score_2 = 0
+score_1 = score_2 = 0
 
 # game loop
 game_loop = True
@@ -58,16 +57,16 @@ while game_loop:
         if event.type == pygame.QUIT:
             game_loop = False
 
-        #  keystroke events
+        # keystroke events
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP:
+            if event.key == pygame.K_w:
                 player_1_move_up = True
-            if event.key == pygame.K_DOWN:
+            if event.key == pygame.K_s:
                 player_1_move_down = True
         if event.type == pygame.KEYUP:
-            if event.key == pygame.K_UP:
+            if event.key == pygame.K_w:
                 player_1_move_up = False
-            if event.key == pygame.K_DOWN:
+            if event.key == pygame.K_s:
                 player_1_move_down = False
 
     # checking the victory condition
@@ -75,48 +74,46 @@ while game_loop:
 
         # clear screen
         screen.fill(COLOR_BLACK)
+        # ball movement
+        ball_x += ball_speed_x
+        ball_y += ball_speed_y
 
         # ball collision with the wall
         if ball_y > 700:
-            ball_dy *= -1
+            ball_speed_y *= -1
             bounce_sound_effect.play()
         elif ball_y <= 0:
-            ball_dy *= -1
+            ball_speed_y *= -1
             bounce_sound_effect.play()
 
         # ball collision with the player 1 's paddle
-        if ball_x < 100:
-            if player_1_y < ball_y + 25:
-                if player_1_y + 150 > ball_y:
-                    ball_dx *= -1
-                    bounce_sound_effect.play()
+        if (ball_x < 100) and (player_1_y < ball_y + 25) and (player_1_y + 150 > ball_y):
+            ball_speed_x *= -1
+            ball_speed_y = random.randrange(-20, 22)
+            bounce_sound_effect.play()
+            print(ball_speed_y)
 
         # ball collision with the player 2 's paddle
-        if ball_x > 1160:
-            if player_2_y < ball_y + 25:
-                if player_2_y + 150 > ball_y:
-                    ball_dx *= -1
-                    bounce_sound_effect.play()
+        if (ball_x > 1160) and (player_2_y < ball_y + 25) and (player_2_y + 150 > ball_y):
+            ball_speed_x *= -1
+            ball_speed_y = random.randrange(-20, 22)
+            bounce_sound_effect.play()
 
         # scoring points
         if ball_x < -50:
             ball_x = 640
             ball_y = 360
-            ball_dy *= -1
-            ball_dx *= -1
+            ball_speed_y *= -1
+            ball_speed_x *= -1
             score_2 += 1
             scoring_sound_effect.play()
         elif ball_x > 1320:
             ball_x = 640
             ball_y = 360
-            ball_dy *= -1
-            ball_dx *= -1
+            ball_speed_y *= -1
+            ball_speed_x *= -1
             score_1 += 1
             scoring_sound_effect.play()
-
-        # ball movement
-        ball_x = ball_x + ball_dx
-        ball_y = ball_y + ball_dy
 
         # player 1 up movement
         if player_1_move_up:
@@ -139,7 +136,7 @@ while game_loop:
             player_1_y = 570
 
         # player 2 "Artificial Intelligence"
-        player_2_y = ball_y
+        player_2_y = ball_y + ball_speed_y
         if player_2_y <= 0:
             player_2_y = 0
         elif player_2_y >= 570:
