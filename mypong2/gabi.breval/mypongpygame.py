d12faedbb739ada6f5a059b624adcd64e5817d7b
gabi.breval <gbdos.eng20@uea.edu.br>
2021-02-10 14:55:27 -0400

    IA

--- mypong2/gabi.breval/mypongpygame.py
+++ mypong2/gabi.breval/mypongpygame.py
@@ -1,16 +1,17 @@
 import pygame
-
+from sklearn import tree
 pygame.init()
 
 '''
 Adress: photos
 /home/gabibreval/Documentos/stem-games/mypong2/assets
+
 '''
 
 COLOR_BLACK = (0, 0, 0)
 COLOR_WHITE = (255, 255, 255)
 
-SCORE_MAX = 1
+SCORE_MAX = 10
 
 # Main window
 screen_width = 1280
@@ -60,6 +61,38 @@ score_1 = score_2 = 0
 game_loop = True
 game_clock = pygame.time.Clock()
 
+def IA():
+    global ball_x, ball_y, player_2_y
+    x = [[985, 705], [255, 705], [665, 705], [640, 0], [960, 0], [230, 0]]
+    y = [0, 0, 0, 1, 1, 1]
+
+    clf = tree.DecisionTreeClassifier()  # creating an object from Tree decision
+    clf = clf.fit(x, y)  # training the computador
+
+    position = clf.predict([[ball_x, ball_y]])  # returns value 1 or 0
+
+
+    if position == 0:
+        player_2_y += 5
+    if position == 1:
+        player_2_y -= 5
+
+
+    if player_2_y <= 0:
+        player_2_y = 0
+    elif player_2_y >= 570:
+        player_2_y = 570
+
+    return position
+
+
+
+'''
+if IA function returns "1" it means that the ball is on the bottom wall
+if IA function returns "0" it means that the ball in on the top of the screen
+
+'''
+print("jogo novo")
 while game_loop:
 
     for event in pygame.event.get():
@@ -79,17 +112,21 @@ while game_loop:
                 player_1_move_down = False
 
     # checking the victory condition
+
     if score_1 < SCORE_MAX and score_2 < SCORE_MAX:
 
+        print(ball_x, ball_y, IA())
         # clear screen
         screen.fill(COLOR_BLACK)
 
         # ball collision with the wall
         if ball_y > 700:
             ball_dy *= -1
+            # print(ball_x, ball_y)  # when ball hits the top
             bounce_sound_effect.play()
         elif ball_y <= 0:
             ball_dy *= -1
+            # print(ball_x, ball_y)  # when ball hits the bottom
             bounce_sound_effect.play()
 
         # ball collision with the player 1 's paddle
@@ -113,13 +150,16 @@ while game_loop:
             ball_dy *= -1
             ball_dx *= -1
             score_2 += 1
+            player_2_y = player_1_y = 300
             scoring_sound_effect.play()
+
         elif ball_x > 1320:
             ball_x = 640
             ball_y = 360
             ball_dy *= -1
             ball_dx *= -1
             score_1 += 1
+            player_2_y = player_1_y = 300
             scoring_sound_effect.play()
 
         # ball movement
@@ -147,11 +187,14 @@ while game_loop:
             player_1_y = 570
 
         # player 2 "Artificial Intelligence"
+        IA()
+        '''
         player_2_y = ball_y
         if player_2_y <= 0:
             player_2_y = 0
         elif player_2_y >= 570:
             player_2_y = 570
+        '''
 
         # update score hud
         score_text = score_font.render(str(score_1) + '     ' + str(score_2), True, COLOR_WHITE, COLOR_BLACK)
