c0936d8495ee6750a213096a432e46de34f673fc
Arthur Gustavo Paiva Carvalho <agpc.eng20@uea.edu.br>
2021-02-10 15:38:54 -0400

    increase of AI.

--- mypong2/arthur.carvalho/mypongpygame.py
+++ mypong2/arthur.carvalho/mypongpygame.py
@@ -2,17 +2,20 @@ import pygame
 
 
 def score_points():
-    global ball_x, ball_y, ball_dx, ball_dy, score_1, score_2
+    global ball_x, ball_y, ball_dx, ball_dy, score_1, score_2, score_text
+    score_text = score_font.render(str(score_1) + ' x ' + str(score_2), True, white, black)
+
     if ball_x == 0:
-        ball_x, ball_y = 510, 320
-        ball_dy *= -1
-        ball_dx *= -1
         score_2 += 1
-    elif ball_x == 1020:
-        ball_x, ball_y = 510, 320
         ball_dy *= -1
         ball_dx *= -1
+        ball_x, ball_y = 510, 320
+
+    elif ball_x == 1020:
         score_1 += 1
+        ball_dy *= -1
+        ball_dx *= -1
+        ball_x, ball_y = 510, 320
 
 
 def draw_objects():
@@ -32,12 +35,32 @@ def move_n_collision_ball():
     if (ball_y < 0) or (ball_y > 640):
         ball_dy *= -1
 
-    if (ball_x > 1020) or (ball_x < 0):
-        ball_dx *= -1
+    else:
+        if (ball_x == 30 and player_1_y + 150 > ball_y > player_1_y) or (ball_x == 970 and player_2_y + 150 > ball_y > player_2_y):
+            ball_dx *= -1
+
+    draw_objects()
+
+
+def artificial_intelligence():
+    global player_2_y
+    if ball_dx > 0:
+        if (ball_dy < 0 and ball_y > player_2_y + 75) or (ball_dy > 0 and ball_y > player_2_y + 75):
+            player_2_y += 1
+
+        elif (ball_dy < 0 and ball_y < player_2_y + 75) or (ball_dy > 0 and ball_y < player_2_y + 75):
+            player_2_y -= 1
+
+        if player_2_y <= 0:
+            player_2_y = 0
+
+        if player_2_y >= 490:
+            player_2_y = 490
 
 
 pygame.init()
 
+black, white = (0, 0, 0), (255, 255, 255)
 score_max = 10
 score_1 = score_2 = 0
 
@@ -55,17 +78,17 @@ player_1_move_up = player_1_move_down = False
 # ball configuration
 ball = pygame.image.load("C:/Users/arthu/Documents/STEM/stem-games/mypong2/assets/arthur.carvalho_ball.png")
 ball_x, ball_y = 510, 320
-ball_dx = ball_dy = 1
+ball_dx = ball_dy = 2
 
 # score text
 score_font = pygame.font.Font('C:/Users/arthu/Documents/STEM/stem-games/mypong2/assets/PressStart2P.ttf', 44)
-score_text = score_font.render('00 x 00', True, (255, 255, 255), (0, 0, 0))
+score_text = score_font.render('00 x 00', True, white, black)
 score_text_rect = score_text.get_rect()
 score_text_rect.center = (550, 30)
 
 # victory text
 victory_font = pygame.font.Font('C:/Users/arthu/Documents/STEM/stem-games/mypong2/assets/PressStart2P.ttf', 70)
-victory_text = victory_font .render('VICTORY', True, (255, 255, 255), (0, 0, 0))
+victory_text = victory_font .render('VICTORY', True, white, black)
 victory_text_rect = score_text.get_rect()
 victory_text_rect.center = (430, 300)
 
@@ -73,7 +96,7 @@ victory_text_rect.center = (430, 300)
 game_loop = True
 
 while game_loop:
-    screen.fill((0, 0, 0))
+    screen.fill(black)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -83,37 +106,38 @@ while game_loop:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_UP:
                 player_1_move_up = True
-            if event.key == pygame.K_DOWN:
+
+            elif event.key == pygame.K_DOWN:
                 player_1_move_down = True
+
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_UP:
                 player_1_move_up = False
-            if event.key == pygame.K_DOWN:
+
+            elif event.key == pygame.K_DOWN:
                 player_1_move_down = False
 
-    # player 1 up and down movement
-    if player_1_move_up:
-        player_1_y -= 1
-
-    if player_1_move_down:
-        player_1_y += 1
-
-    # player 1 collides with upper wall
-    if player_1_y <= 0:
-        player_1_y = 0
-
-    # player 1 collides with lower wall
-    elif player_1_y >= 490:
-        player_1_y = 490
-
     if score_1 < score_max and score_2 < score_max:
+        artificial_intelligence()
         move_n_collision_ball()
         score_points()
-        draw_objects()
-        score_text = score_font.render(str(score_1) + ' x ' + str(score_2), True, (255, 255, 255), (0, 0, 0))
+
+        # player 1 up and down movement
+        if player_1_move_up:
+            player_1_y -= 1
+
+            # player 1 collides with upper wall
+            if player_1_y <= 0:
+                player_1_y = 0
+
+        elif player_1_move_down:
+            player_1_y += 1
+
+            # player 1 collides with lower wall
+            if player_1_y >= 490:
+                player_1_y = 490
+
     else:
-        screen.fill((0, 0, 0))
-        screen.blit(score_text, score_text_rect)
         screen.blit(victory_text, victory_text_rect)
         pygame.display.update()
 
