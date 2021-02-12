52168e8bf048ca7fa23f82605037068ef906954c
Natanael Lucena <nldm.eng20@uea.edu.br>
2021-02-12 10:47:39 -0400

    Code organized into functions. Also most numeric values are now varying according to the game screen size and ball random collision with AI fixed

--- mypong2/natanael.lucena/mypongpygame.py
+++ mypong2/natanael.lucena/mypongpygame.py
@@ -3,9 +3,14 @@ from random import uniform
 
 pygame.init()
 
-
 COLOR_LIGHT_GREY = (200, 200, 200)
 COLOR_DARK_GREY = pygame.Color('gray12')
+
+
+def font(fontsize):
+    font_path = '../assets/PressStart2P.ttf'
+    return pygame.font.Font(font_path, fontsize)
+
 
 SCORE_MAX = 2
 
@@ -14,38 +19,56 @@ screen = pygame.display.set_mode(size)
 
 pygame.display.set_caption("Pong")
 
-player_score = 0
-ia_score = 0
+score_font = font(42)
+result_font = font(100)
 
-# score text
-score_font = pygame.font.Font('../assets/PressStart2P.ttf', 40)
-
-# victory text
-victory_font = pygame.font.Font('../assets/PressStart2P.ttf', 100)
 
 # sound effects
-bounce_sound_effect = pygame.mixer.Sound('../assets/bounce.wav')
-scoring_sound_effect = pygame.mixer.Sound('../assets/258020__kodack__arcade-bleep-sound.wav')
+def sound(name):
+    sound_path = "../assets/258020__kodack__arcade-bleep-sound.wav"
+    if name == "bounce":
+        sound_path = "../assets/bounce.wav"
+    return pygame.mixer.Sound(sound_path)
+
+
+bounce_sound_effect = sound("bounce")
+scoring_sound_effect = sound("score")
+
+
+def image(name):
+    image_path = "../assets/natanael.lucena/natanael.lucena_paddle.png"
+    if name == "ball":
+        image_path = "../assets/natanael.lucena/natanael.lucena_ball.png"
+    return pygame.image.load(image_path)
+
+
+def set_obj_coordinates(obj, x, y):
+    obj.x = x
+    obj.y = y
+
 
 # player 1
-player_1_img = pygame.image.load("../assets/natanael.lucena/natanael.lucena_paddle.png")
+player_1_img = image("paddle")
 player_1 = player_1_img.get_rect()
-player_1.x = 60
-player_1.y = 300
+set_obj_coordinates(player_1, 60, (size[1] - player_1.height) / 2)
 player_1_move_up = False
 player_1_move_down = False
+player_score = 0
 
 # player 2 - robot
-player_2_img = pygame.image.load("../assets/natanael.lucena/natanael.lucena_paddle.png")
-player_2 = player_2_img.get_rect()
-player_2.x = 1190
-player_2.y = 300
+ai_img = image("paddle")
+ai = ai_img.get_rect()
+set_obj_coordinates(ai, size[0] - ai.width - player_1.x, (size[1] - ai.height) / 2)
+ai_score = 0
+ai_move_up = False
+ai_move_down = False
+ai_difficult = {"easy": False, "normal": False, "hard": False, "impossible": True}
+# ia_dy = 5
 
 # ball
-ball_img = pygame.image.load("../assets/natanael.lucena/natanael.lucena_ball.png")
+ball_img = image("ball")
 ball = ball_img.get_rect()
-ball.x = 640
-ball.y = 360
+set_obj_coordinates(ball, size[0] / 2, size[1] / 2)
 ball_dx = 5
 ball_dy = uniform(-2.14, 2.14) * 5
 
@@ -53,141 +76,156 @@ ball_dy = uniform(-2.14, 2.14) * 5
 game_loop = True
 game_clock = pygame.time.Clock()
 
+
+def check_key(b):
+    global player_1_move_up, player_1_move_down, event
+    if event.key == pygame.K_w:
+        player_1_move_up = b
+    if event.key == pygame.K_s:
+        player_1_move_down = b
+
+
+def event_conditional():
+    global game_loop, event
+    if event.type == pygame.QUIT:
+        game_loop = False
+    elif event.type == pygame.KEYDOWN:
+        check_key(True)
+    elif event.type == pygame.KEYUP:
+        check_key(False)
+
+
+def text_render(score_text):
+    return score_font.render(f"{score_text}", True, COLOR_LIGHT_GREY)
+
+
+def random_angle(b1, b2):
+    if b1:
+        num = uniform(1, 2.14) * 5
+    elif b2:
+        num = uniform(-2.14, -1) * 5
+    else:
+        num = 5
+    return num
+
+
+def random_speed(paddle):
+    aux = 1
+    if paddle == ai:
+        aux *= -1
+    if paddle.bottomright[1] / 3 <= ball.y <= paddle.bottomright[1] / 1.5:
+        num = uniform(5 * aux, 6 * aux)
+    else:
+        num = uniform(5 * aux, 10 * aux)
+    return num
+
+
+def paddle_collision(paddle):
+    global ball_dy, ball_dx, ai_move_up, ai_move_down
+    expression = "< 0"
+    b1, b2 = player_1_move_down, player_1_move_up
+    if paddle == ai:
+        expression = "> 0"
+        if ai_difficult["impossible"]:
+            if ball_dy < 0:
+                ai_move_up = True
+            else:
+                ai_move_down = True
+        b1, b2 = ai_move_down, ai_move_up
+    if ball.colliderect(paddle) and eval("ball_dx " + expression):
+        if (abs(paddle.top - ball.bottom) < collision_tolerance and ball_dy > 0) or (
+                abs(paddle.bottom - ball.top) < collision_tolerance and ball_dy < 0):
+            ball_dy *= -1
+        if abs(paddle.right - ball.left) < collision_tolerance:
+            ball_dy = random_angle(b1, b2)
+        ball_dx = random_speed(paddle)
+        bounce_sound_effect.play()
+
+
+def score_point_treatment():
+    global ball_dy, ball_dx
+    ball.x = size[0] / 2
+    ball.y = size[1] / 2
+    ball_dy *= -1
+    ball_dx *= -1
+    scoring_sound_effect.play()
+
+
+def paddle_wall_collision(paddle):
+    if paddle.y <= 0:
+        paddle.y = 0
+    elif paddle.y >= size[1] - player_1.height:
+        paddle.y = size[1] - player_1.height
+
+
+def draw_scenario():
+    screen.fill(COLOR_DARK_GREY)
+    screen.blit(player_text, (size[0] / 2 - 110, 50))
+    screen.blit(ia_text, (size[0] * 9 / 16, 50))
+
+
 while game_loop:
-
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            game_loop = False
-
-        #  keystroke events
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_w:
-                player_1_move_up = True
-            if event.key == pygame.K_s:
-                player_1_move_down = True
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_w:
-                player_1_move_up = False
-            if event.key == pygame.K_s:
-                player_1_move_down = False
-    player_text = score_font.render(f"{player_score}", True, COLOR_LIGHT_GREY)
-    ia_text = score_font.render(f"{ia_score}", True, COLOR_LIGHT_GREY)
+        event_conditional()
+    player_text = text_render(player_score)
+    ia_text = text_render(ai_score)
     # checking the victory condition
-    if player_score < SCORE_MAX and ia_score < SCORE_MAX:
+    if player_score < SCORE_MAX and ai_score < SCORE_MAX:
 
         # clear screen
         screen.fill(COLOR_DARK_GREY)
 
         # ball collision with the wall
-        if ball.y > 700:
-            ball_dy *= -1
-            bounce_sound_effect.play()
-        elif ball.y <= 0:
+        if ball.y >= size[1] - ball.height or ball.y <= 0:
             ball_dy *= -1
             bounce_sound_effect.play()
 
         collision_tolerance = 10
-        if ball.colliderect(player_1) and ball_dx < 0:
-            if abs(player_1.top - ball.bottom) < collision_tolerance and ball_dy > 0:
-                ball_dy *= -1
-            if abs(player_1.bottom - ball.top) < collision_tolerance and ball_dy < 0:
-                ball_dy *= -1
-            if abs(player_1.right - ball.left) < collision_tolerance:
-                if player_1_move_down:
-                    ball_dy = uniform(1, 2.14) * 5
-                elif player_1_move_up:
-                    ball_dy = uniform(-2.14, -1) * 5
-            if player_1.bottomright[1] / 3 <= ball.y <= player_1.bottomright[1] / 1.5:
-                ball_dx = uniform(5, 6)
-            else:
-                ball_dx = uniform(5, 10)
-            bounce_sound_effect.play()
-
-        # ball collision with the player 2 's paddle
-        if ball.colliderect(player_2) and ball_dx > 0:
-            if abs(player_2.top - ball.bottom) < collision_tolerance and ball_dy > 0:
-                ball_dy *= -1
-            if abs(player_2.bottom - ball.top) < collision_tolerance and ball_dy < 0:
-                ball_dy *= -1
-            if abs(player_2.left - ball.right) < collision_tolerance:
-                if player_1_move_down:
-                    ball_dy = uniform(1, 2.14) * 5
-                elif player_1_move_up:
-                    ball_dy = uniform(-2.14, -1) * 5
-            if player_2.bottomright[1] / 3 <= ball.y <= player_2.bottomright[1] / 1.5:
-                ball_dx = uniform(-6, -5)
-            else:
-                ball_dx = uniform(-10, -5)
-            bounce_sound_effect.play()
+        paddle_collision(player_1)
+        paddle_collision(ai)
 
         # scoring points
         if ball.x < -50:
-            ball.x = 640
-            ball.y = 360
-            ball_dy *= -1
-            ball_dx *= -1
-            ia_score += 1
-            scoring_sound_effect.play()
-        elif ball.x > 1320:
-            ball.x = 640
-            ball.y = 360
-            ball_dy *= -1
-            ball_dx *= -1
+            score_point_treatment()
+            ai_score += 1
+        elif ball.x > size[0] + 50:
+            score_point_treatment()
             player_score += 1
-            scoring_sound_effect.play()
 
         # ball movement
-        ball.x = ball.x + ball_dx
-        ball.y = ball.y + ball_dy
+        ball.x += ball_dx
+        ball.y += ball_dy
 
         # player 1 up movement
         if player_1_move_up:
             player_1.y -= 5
-        else:
-            player_1.y += 0
 
         # player 1 down movement
         if player_1_move_down:
             player_1.y += 5
-        else:
-            player_1.y += 0
-
-        # player 1 collides with upper wall
-        if player_1.y <= 0:
-            player_1.y = 0
-
-        # player 1 collides with lower wall
-        elif player_1.y >= 570:
-            player_1.y = 570
+        paddle_wall_collision(player_1)
 
         # player 2 "Artificial Intelligence"
-        player_2.y = ball.y
-        if player_2.y <= 0:
-            player_2.y = 0
-        elif player_2.y >= 570:
-            player_2.y = 570
-
-        # update score hud
+        ai.y = ball.y
+        paddle_wall_collision(ai)
 
         # drawing objects
-        screen.fill(COLOR_DARK_GREY)
+        draw_scenario()
+        pygame.draw.aaline(screen, COLOR_LIGHT_GREY, (size[0] / 2, 0), (size[0] / 2, size[1]))
         screen.blit(ball_img, ball)
         screen.blit(player_1_img, player_1)
-        screen.blit(player_2_img, player_2)
-        screen.blit(player_text, (530, 50))
-        screen.blit(ia_text, (730, 50))
-        pygame.draw.aaline(screen, (200, 200, 200), (640, 0), (640, 720))
+        screen.blit(ai_img, ai)
 
     else:
         # drawing victory
         result = "VICTORY!"
-        screen.fill(COLOR_DARK_GREY)
-        if player_score < ia_score:
+        text_position = (size[1] / 2 - 70, size[1] / 2 - 70)
+        if player_score < ai_score:
+            text_position = (size[1] / 2, text_position[1])
             result = "DEFEAT"
-        victory_text = victory_font.render(result, True, COLOR_LIGHT_GREY)
-        screen.blit(player_text, (530, 50))
-        screen.blit(ia_text, (730, 50))
-        screen.blit(victory_text, (360, 290))
+        result_text = result_font.render(result, True, COLOR_LIGHT_GREY)
+        draw_scenario()
+        screen.blit(result_text, text_position)
 
     # update screen
     pygame.display.flip()
