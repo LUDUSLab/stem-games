c035ad5d160da06e4cda0c0a28a7fa9e1650d276
Ronald Boadana <rcb.eng20@uea.edu.br>
2021-02-12 10:35:34 -0400

    i changed the directory which the font, sound and images is; i added the condition what the player can win the ai and the screen will show 'victory' and when the ai win the screen show 'game over'; and functions that make the ai loss some concentration

--- mypong2/ronald.boadana/mypongpygame.py
+++ mypong2/ronald.boadana/mypongpygame.py
@@ -1,5 +1,5 @@
 import pygame
-import random  # for ball randomness
+import random  # for ball randomness and 'A.I.' loss any concentration
 # Powered by Ronald Boadana
 
 pygame.init()
@@ -11,37 +11,42 @@ SCORE_MAX = 10
 
 size = (1280, 720)
 screen = pygame.display.set_mode(size)
-pygame.display.set_caption("MyPong2.0 Alpha 0.5 - PyGame Edition - 2021.02.11")
+pygame.display.set_caption("Pong 2.0 (Beta 1.0) - PyGame Edition - 2021.02.12")
 
 # score text
-score_font = pygame.font.Font('C:/Users/bh_ro/Documents/STEM/stem-games/mypong2/assets/PressStart2P.ttf', 44)
+score_font = pygame.font.Font('../assets/PressStart2P.ttf', 44)
 score_text = score_font.render('00 x 00', True, COLOR_WHITE, COLOR_BLACK)
 score_text_rect = score_text.get_rect()
 score_text_rect.center = (680, 50)
 
 # victory text
-victory_font = pygame.font.Font('C:/Users/bh_ro/Documents/STEM/stem-games/mypong2/assets/PressStart2P.ttf', 100)
+victory_font = pygame.font.Font('../assets/PressStart2P.ttf', 100)
 victory_text = victory_font.render('VICTORY', True, COLOR_WHITE, COLOR_BLACK)
 victory_text_rect = score_text.get_rect()
 victory_text_rect.center = (450, 350)
 
+# defeat text
+defeat_font = pygame.font.Font('../assets/PressStart2P.ttf', 100)
+defeat_text = victory_font.render('GAME OVER', True, COLOR_WHITE, COLOR_BLACK)
+defeat_text_rect = score_text.get_rect()
+defeat_text_rect.center = (350, 350)
+
 # sound effects
-bounce_sound_effect = pygame.mixer.Sound('C:/Users/bh_ro/Documents/STEM/stem-games/mypong2/assets/bounce.wav')
-scoring_sound_effect = pygame.mixer.Sound('C:/Users/bh_ro/Documents/STEM/stem-games/mypong2/assets'
-                                          '/258020__kodack__arcade-bleep-sound.wav')
+bounce_sound_effect = pygame.mixer.Sound('../assets/bounce.wav')
+scoring_sound_effect = pygame.mixer.Sound('../assets/258020__kodack__arcade-bleep-sound.wav')
 
 # player 1
-player_1 = pygame.image.load('C:/Users/bh_ro/Documents/STEM/stem-games/mypong2/assets/ronald.boadana_paddle.png')
+player_1 = pygame.image.load('../assets/ronald.boadana_paddle.png')
 player_1_y = 300
 player_1_move_up = False
 player_1_move_down = False
 
-# player 2 - robot
-player_2 = pygame.image.load('C:/Users/bh_ro/Documents/STEM/stem-games/mypong2/assets/ronald.boadana_paddle.png')
-player_2_y = 300
+# 'A.I.'
+ai_player = pygame.image.load('../assets/ronald.boadana_paddle.png')
+ai_player_y = 300
 
 # ball
-ball = pygame.image.load('C:/Users/bh_ro/Documents/STEM/stem-games/mypong2/assets/ronald.boadana_ball.png')
+ball = pygame.image.load('../assets/ronald.boadana_ball.png')
 ball_x, ball_y = 640, 360
 ball_speed_x = ball_speed_y = 5
 
@@ -92,14 +97,12 @@ while game_loop:
             ball_speed_x *= -1
             ball_speed_y = random.randrange(-15, 16)
             bounce_sound_effect.play()
-            print(ball_speed_y)
 
         # ball collision with the player 2 's paddle
-        if (ball_x == 1190) and (player_2_y < ball_y + 25) and (player_2_y + 150 > ball_y):
+        if (ball_x == 1190) and (ai_player_y < ball_y + 25) and (ai_player_y + 150 > ball_y):
             ball_speed_x *= -1
             ball_speed_y = random.randrange(-15, 16)
             bounce_sound_effect.play()
-            print(ball_speed_y)
 
         # player 2 scores points
         if ball_x < -50:
@@ -138,12 +141,19 @@ while game_loop:
         elif player_1_y >= 570:
             player_1_y = 570
 
-        # player 2 "Artificial Intelligence"
-        player_2_y = ball_y + ball_speed_y
-        if player_2_y <= 0:
-            player_2_y = 0
-        elif player_2_y >= 570:
-            player_2_y = 570
+        # "Artificial Intelligence" loss concentration sometimes
+        if ball_y < ai_player_y + 5 and random.randrange(31) == 10:
+            ai_player_y = ball_y + 5
+        if ball_y > ai_player_y - 5 and random.randrange(31) == 10:
+            ai_player_y = ball_y - 5
+
+        # 'A.I.' collides with upper wall
+        if ai_player_y <= 0:
+            ai_player_y = 0
+
+        # 'A.I.' collides with lover wall
+        elif ai_player_y >= 570:
+            ai_player_y = 570
 
         # update score hud
         score_text = score_font.render(str(score_1) + ' x ' + str(score_2), True, COLOR_WHITE, COLOR_BLACK)
@@ -151,13 +161,15 @@ while game_loop:
         # drawing objects
         screen.blit(ball, (ball_x, ball_y))
         screen.blit(player_1, (50, player_1_y))
-        screen.blit(player_2, (1200, player_2_y))
+        screen.blit(ai_player, (1200, ai_player_y))
         screen.blit(score_text, score_text_rect)
     else:
         # drawing victory
         screen.fill(COLOR_BLACK)
-        screen.blit(score_text, score_text_rect)
-        screen.blit(victory_text, victory_text_rect)
+        if score_1 == 10:
+            screen.blit(victory_text, victory_text_rect)
+        elif score_2 == 10:
+            screen.blit(defeat_text, defeat_text_rect)
 
     # update screen
     pygame.display.flip()
