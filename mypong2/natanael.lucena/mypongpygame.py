2b19a6c1599e2cb01364fa2631ba0d839949b9a2
Natanael Lucena <nldm.eng20@uea.edu.br>
2021-02-13 12:46:41 -0400

    Game menu, player vs player and player vs computer modes added.

--- mypong2/natanael.lucena/mypongpygame.py
+++ mypong2/natanael.lucena/mypongpygame.py
@@ -3,6 +3,7 @@ from random import uniform
 
 pygame.init()
 
+COLOR_BLACK = pygame.Color('black')
 COLOR_LIGHT_GREY = (200, 200, 200)
 COLOR_DARK_GREY = pygame.Color('gray12')
 
@@ -21,6 +22,8 @@ pygame.display.set_caption("Pong")
 
 score_font = font(42)
 result_font = font(100)
+header_font = font(64)
+button_font = font(26)
 
 
 # sound effects
@@ -53,16 +56,16 @@ player_1 = player_1_img.get_rect()
 set_obj_coordinates(player_1, 60, (size[1] - player_1.height) / 2)
 player_1_move_up = False
 player_1_move_down = False
-player_score = 0
+player_1_score = 0
 
 # player 2 - robot
-ai_img = image("paddle")
-ai = ai_img.get_rect()
-set_obj_coordinates(ai, size[0] - ai.width - player_1.x, (size[1] - ai.height) / 2)
-ai_score = 0
-ai_move_up = False
-ai_move_down = False
-ai_difficult = {"easy": False, "normal": False, "hard": False, "impossible": True}
+player_2_img = image("paddle")
+player_2 = player_2_img.get_rect()
+set_obj_coordinates(player_2, size[0] - player_2.width - player_1.x, (size[1] - player_2.height) / 2)
+player_2_score = 0
+player_2_move_up = False
+player_2_move_down = False
+player_2_difficult = {"easy": False, "normal": False, "hard": False, "impossible": False}
 # ia_dy = 5
 
 # ball
@@ -74,25 +77,52 @@ ball_dy = uniform(-2.14, 2.14) * 5
 
 # game loop
 game_loop = True
+menu_loop = True
+mode_selected = False
+menu_clock = pygame.time.Clock()
 game_clock = pygame.time.Clock()
 
 
-def check_key(b):
-    global player_1_move_up, player_1_move_down, event
+def check_player_1_key(b):
+    global player_1_move_up, player_1_move_down
     if event.key == pygame.K_w:
         player_1_move_up = b
     if event.key == pygame.K_s:
         player_1_move_down = b
 
 
+def check_player_2_key(b):
+    global player_2_move_up, player_2_move_down
+    if event.key == pygame.K_UP:
+        player_2_move_up = b
+    if event.key == pygame.K_DOWN:
+        player_2_move_down = b
+
+
 def event_conditional():
-    global game_loop, event
+    global game_loop, menu_loop
     if event.type == pygame.QUIT:
         game_loop = False
+        menu_loop = False
     elif event.type == pygame.KEYDOWN:
-        check_key(True)
+        check_player_1_key(True)
+        check_player_2_key(True)
     elif event.type == pygame.KEYUP:
-        check_key(False)
+        check_player_1_key(False)
+        check_player_2_key(False)
+
+
+def menu_listen_select():
+    global mode_selected, pvp_button, pvc_button
+    if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_RETURN:
+            mode_selected = True
+    elif event.type == pygame.KEYUP:
+        if event.key == pygame.K_RETURN:
+            mode_selected = False
+    if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+        if pvc_button.collidepoint(pygame.mouse.get_pos()) or pvp_button.collidepoint(pygame.mouse.get_pos()):
+            mode_selected = True
 
 
 def text_render(score_text):
@@ -111,7 +141,7 @@ def random_angle(b1, b2):
 
 def random_speed(paddle):
     aux = 1
-    if paddle == ai:
+    if paddle == player_2:
         aux *= -1
     if paddle.bottomright[1] / 3 <= ball.y <= paddle.bottomright[1] / 1.5:
         num = uniform(5 * aux, 6 * aux)
@@ -121,17 +151,17 @@ def random_speed(paddle):
 
 
 def paddle_collision(paddle):
-    global ball_dy, ball_dx, ai_move_up, ai_move_down
+    global ball_dy, ball_dx, player_2_move_up, player_2_move_down
     expression = "< 0"
     b1, b2 = player_1_move_down, player_1_move_up
-    if paddle == ai:
+    if paddle == player_2:
         expression = "> 0"
-        if ai_difficult["impossible"]:
+        if player_2_difficult["impossible"]:
             if ball_dy < 0:
-                ai_move_up = True
+                player_2_move_up = True
             else:
-                ai_move_down = True
-        b1, b2 = ai_move_down, ai_move_up
+                player_2_move_down = True
+        b1, b2 = player_2_move_down, player_2_move_up
     if ball.colliderect(paddle) and eval("ball_dx " + expression):
         if (abs(paddle.top - ball.bottom) < collision_tolerance and ball_dy > 0) or (
                 abs(paddle.bottom - ball.top) < collision_tolerance and ball_dy < 0):
@@ -154,23 +184,65 @@ def score_point_treatment():
 def paddle_wall_collision(paddle):
     if paddle.y <= 0:
         paddle.y = 0
-    elif paddle.y >= size[1] - player_1.height:
-        paddle.y = size[1] - player_1.height
+    elif paddle.y >= size[1] - paddle.height:
+        paddle.y = size[1] - paddle.height
 
 
 def draw_scenario():
     screen.fill(COLOR_DARK_GREY)
     screen.blit(player_text, (size[0] / 2 - 110, 50))
-    screen.blit(ia_text, (size[0] * 9 / 16, 50))
+    screen.blit(player_2_text, (size[0] * 9 / 16, 50))
 
+
+pvp_button = pygame.Rect(size[0] / 2 - (size[0] / 3 + 2) / 2, size[1] * 2 / 5, size[0] / 3 + 2, size[1] / 18)
+pvc_button = pygame.Rect(size[0] / 2 - (size[0] / 3 + 54) / 2, size[1] * 3 / 5, size[0] / 3 + 54, size[1] / 18)
+menu_text = header_font.render("SELECT MODE", True, COLOR_LIGHT_GREY)
+pvp_text = button_font.render("PLAYER VS PLAYER", True, COLOR_LIGHT_GREY)
+pvp_text_selected = button_font.render("PLAYER VS PLAYER", True, COLOR_BLACK)
+pvc_text = button_font.render("PLAYER VS COMPUTER", True, COLOR_LIGHT_GREY)
+pvc_text_selected = button_font.render("PLAYER VS COMPUTER", True, COLOR_BLACK)
+
+menu_buttons = [pvp_button, pvc_button]
+selected_index = 0
+
+pvp_mode = False
+pvc_mode = False
+
+while menu_loop:
+    for event in pygame.event.get():
+        event_conditional()
+        menu_listen_select()
+    screen.fill(COLOR_DARK_GREY)
+    if mode_selected:
+        if selected_index:
+            pvc_mode = True
+        else:
+            pvp_mode = True
+        menu_loop = False
+    if player_1_move_down or pvc_button.collidepoint(pygame.mouse.get_pos()):
+        selected_index = 1
+    if player_1_move_up or pvp_button.collidepoint(pygame.mouse.get_pos()):
+        selected_index = 0
+    pygame.draw.rect(screen, COLOR_LIGHT_GREY, menu_buttons[selected_index])
+    if selected_index:
+        screen.blit(pvc_text_selected, (pvc_button.x + 7, pvc_button.y + 8))
+        screen.blit(pvp_text, (pvp_button.x + 7, pvp_button.y + 8))
+    else:
+        screen.blit(pvp_text_selected, (pvp_button.x + 7, pvp_button.y + 8))
+        screen.blit(pvc_text, (pvc_button.x + 7, pvc_button.y + 8))
+
+    screen.blit(menu_text, (size[0] / 2 - 330, 80))
+
+    pygame.display.flip()
+    menu_clock.tick(60)
 
 while game_loop:
     for event in pygame.event.get():
         event_conditional()
-    player_text = text_render(player_score)
-    ia_text = text_render(ai_score)
+    player_text = text_render(player_1_score)
+    player_2_text = text_render(player_2_score)
     # checking the victory condition
-    if player_score < SCORE_MAX and ai_score < SCORE_MAX:
+    if player_1_score < SCORE_MAX and player_2_score < SCORE_MAX:
 
         # clear screen
         screen.fill(COLOR_DARK_GREY)
@@ -182,15 +254,15 @@ while game_loop:
 
         collision_tolerance = 10
         paddle_collision(player_1)
-        paddle_collision(ai)
+        paddle_collision(player_2)
 
         # scoring points
         if ball.x < -50:
             score_point_treatment()
-            ai_score += 1
+            player_2_score += 1
         elif ball.x > size[0] + 50:
             score_point_treatment()
-            player_score += 1
+            player_1_score += 1
 
         # ball movement
         ball.x += ball_dx
@@ -206,21 +278,28 @@ while game_loop:
         paddle_wall_collision(player_1)
 
         # player 2 "Artificial Intelligence"
-        ai.y = ball.y
-        paddle_wall_collision(ai)
+
+        if pvc_mode:
+            player_2.y = ball.y
+        paddle_wall_collision(player_2)
+        if pvp_mode:
+            if player_2_move_up:
+                player_2.y -= 5
+            if player_2_move_down:
+                player_2.y += 5
 
         # drawing objects
         draw_scenario()
         pygame.draw.aaline(screen, COLOR_LIGHT_GREY, (size[0] / 2, 0), (size[0] / 2, size[1]))
         screen.blit(ball_img, ball)
         screen.blit(player_1_img, player_1)
-        screen.blit(ai_img, ai)
+        screen.blit(player_2_img, player_2)
 
     else:
         # drawing victory
         result = "VICTORY!"
         text_position = (size[1] / 2 - 70, size[1] / 2 - 70)
-        if player_score < ai_score:
+        if player_1_score < player_2_score:
             text_position = (size[1] / 2, text_position[1])
             result = "DEFEAT"
         result_text = result_font.render(result, True, COLOR_LIGHT_GREY)
