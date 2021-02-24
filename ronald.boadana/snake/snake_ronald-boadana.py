4b4884e485af9b23c744458f71647528c5a2d5cd
Ronald Boadana <rcb.eng20@uea.edu.br>
2021-02-24 10:57:18 -0400

    the game is working still very basic, but tomorrow i'm going to make some improvements on code

--- ronald.boadana/snake/snake_ronald-boadana.py
+++ ronald.boadana/snake/snake_ronald-boadana.py
@@ -4,21 +4,41 @@ import random
 
 pygame.init()
 
-color_black = (255, 255, 255)
+color_black = (0, 0, 0)
 screen_size = (600, 600)
 screen = pygame.display.set_mode(screen_size)
-pygame.display.set_caption('Snake v0.1')
+pygame.display.set_caption('Snake v0.2')
 
 snake = pygame.image.load('C:/Users/bh_ro/Documents/STEM/stem-games/mypong2/assets/ronald.boadana_ball.png')
-snake_x, snake_y = 0, 0
+snake_x, snake_y = 300, 300
 
 apple = pygame.image.load('C:/Users/bh_ro/Documents/STEM/stem-games/mypong2/assets/ronald.boadana_ball.png')
 apple_pos = (random.randrange(0, 590), random.randrange(0, 590))
 
+# conditions to snake can move
+direction_x, direction_y = 7, 7
+
 game_on = True
+game_clock = pygame.time.Clock()
 while game_on:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
 
+        # mapping keys
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_w:
+                snake_y -= direction_y
+            if event.key == pygame.K_s:
+                snake_y += direction_y
+            if event.key == pygame.K_a:
+                snake_x -= direction_x
+            if event.key == pygame.K_d:
+                snake_x += direction_x
+
+    screen.blit(snake, (snake_x, snake_y))
+    screen.blit(apple, apple_pos)
     pygame.display.update()
+    # updating screen
+    screen.fill(color_black)
+    game_clock.tick(90)
