4d781fb3c4c2dd7557108e17fca5fe0f5762e92a
Ronald Boadana <rcb.eng20@uea.edu.br>
2021-02-24 09:22:57 -0400

    code in developing, but still it isn't working

--- ronald.boadana/snake/snake_ronald-boadana.py
+++ ronald.boadana/snake/snake_ronald-boadana.py
@@ -1,5 +1,24 @@
 import pygame
-import random  # for ball randomness and 'A.I.' loss any concentration
+import random
 # Powered by Ronald Boadana
 
 pygame.init()
+
+color_black = (255, 255, 255)
+screen_size = (600, 600)
+screen = pygame.display.set_mode(screen_size)
+pygame.display.set_caption('Snake v0.1')
+
+snake = pygame.image.load('C:/Users/bh_ro/Documents/STEM/stem-games/mypong2/assets/ronald.boadana_ball.png')
+snake_x, snake_y = 0, 0
+
+apple = pygame.image.load('C:/Users/bh_ro/Documents/STEM/stem-games/mypong2/assets/ronald.boadana_ball.png')
+apple_pos = (random.randrange(0, 590), random.randrange(0, 590))
+
+game_on = True
+while game_on:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+
+    pygame.display.update()
