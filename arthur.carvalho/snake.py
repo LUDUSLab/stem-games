bf83225a5fdae14b537798bad34efd6750ffbfd3
Arthur Gustavo Paiva Carvalho <agpc.eng20@uea.edu.br>
2021-02-25 11:21:34 -0400

    Changes in snake move.

--- arthur.carvalho/snake.py
+++ arthur.carvalho/snake.py
@@ -1,6 +1,11 @@
 import pygame
 from random import randint
 
+
+def body_snake():
+    screen.blit(snake, snake_position)
+
+
 pygame.init()
 
 # Screen configuration
@@ -11,11 +16,12 @@ pygame.display.set_caption('Snake')
 snake = pygame.Surface((10, 10))
 snake.fill((0, 255, 0))
 snake_position = [250, 250]
+UP = DOWN = LEFT = RIGHT = False
 
 # Apple set
 apple = pygame.Surface((10, 10))
 apple.fill((255, 0, 0))
-apple_position = (randint(0, 490), randint(0, 490))
+apple_position = [randint(0, 490) // 10 * 10, randint(0, 490) // 10 * 10]
 
 game_loop = True
 
@@ -28,24 +34,39 @@ while game_loop:
         # map keys
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_UP:
-                snake_position[1] -= 10
-                print(snake_position)
+                UP = True
+                DOWN = LEFT = RIGHT = False
 
             elif event.key == pygame.K_DOWN:
-                snake_position[1] += 10
-                print(snake_position)
+                DOWN = True
+                UP = LEFT = RIGHT = False
 
             elif event.key == pygame.K_LEFT:
-                snake_position[0] -= 10
-                print(snake_position)
+                LEFT = True
+                UP = DOWN = RIGHT = False
 
             elif event.key == pygame.K_RIGHT:
-                snake_position[0] += 10
-                print(snake_position)
+                RIGHT = True
+                UP = DOWN = LEFT = False
+
+    if UP:
+        snake_position[1] -= 0.1
+
+    elif DOWN:
+        snake_position[1] += 0.1
+
+    elif LEFT:
+        snake_position[0] -= 0.1
+
+    elif RIGHT:
+        snake_position[0] += 0.1
+
+    if snake_position[0] == apple_position:
+        apple_position = [randint(0, 490) // 10 * 10, randint(0, 490) // 10 * 10]
 
     screen.fill((0, 0, 0))
 
     screen.blit(apple, apple_position)
-    screen.blit(snake, snake_position)
+    body_snake()
 
     pygame.display.update()
