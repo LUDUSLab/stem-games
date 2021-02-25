cfefd1671dddb38451a08b3c4e06947ecf0002de
Ronald Boadana <rcb.eng20@uea.edu.br>
2021-02-25 11:46:14 -0400

    i added somethings on code, but the x and y of snake never is equal to apple position, this is a big problem, but i'll fix; i added the score but it isn't working yet

--- ronald.boadana/snake/snake_ronald-boadana.py
+++ ronald.boadana/snake/snake_ronald-boadana.py
@@ -5,27 +5,32 @@ import random
 
 pygame.init()
 
-color_white = (0, 0, 0)
-color_black = (255, 255, 255)
+max_score = 10
+player_score = 0
+
+color_black = (0, 0, 0)
+color_white = (255, 255, 255)
 screen_size = (600, 600)
 screen = pygame.display.set_mode(screen_size)
-pygame.display.set_caption('Snake v0.2')
+pygame.display.set_caption('Snake v0.5')
 
+# drawing snake
 snake = pygame.image.load('../snake/assets/ronald.boadana_snakehead.png')
 snake_x, snake_y = 300, 300
 snake_pos = (snake_x, snake_y)
 
+# drawing apple
 apple = pygame.image.load('../snake/assets/ronald.boadana_apple.png')
-apple_pos = (random.randrange(0, 590), random.randrange(0, 590))
+apple_pos = (random.randint(0, 590)//10 * 10, random.randint(0, 590)//10 * 10)
 
 # score text
-score_font = pygame.font.Font('../assets/PressStart2P.ttf', 44)
+score_font = pygame.font.Font('../snake/assets/PressStart2P.ttf', 30)
 score_text = score_font.render('0', True, color_white, color_black)
 score_text_rect = score_text.get_rect()
-score_text_rect.center = (680, 50)
+score_text_rect.center = (300, 25)
 
 # defeat text
-defeat_font = pygame.font.Font('../assets/PressStart2P.ttf', 100)
+defeat_font = pygame.font.Font('../snake/assets/PressStart2P.ttf', 60)
 defeat_text = defeat_font.render('GAME OVER', True, color_white, color_black)
 defeat_text_rect = score_text.get_rect()
 defeat_text_rect.center = (350, 350)
@@ -51,9 +56,25 @@ while game_on:
             if event.key == pygame.K_d:
                 snake_x += direction_x
 
-        if (snake_x == 0) or 600 and (snake_y == 0 or 600)
+    # victory condition
+    if player_score < max_score:
+        if snake_x == apple_pos:
+            player_score += 1
+            apple_pos = (random.randint(0, 590)//10*10, random.randint(0, 590)//10*10)
+            print(apple_pos)
+            print(snake_x)
+            print(snake_y)
+
+        score_text = score_font.render(str(player_score), True, color_white, color_black)
+
+    else:
+        if (snake_x == 0 or snake_y == 0) and (snake_x == 600 or snake_y == 600):
+            screen.blit(defeat_text, defeat_text_rect)
+
+    screen.blit(score_text, score_text_rect)
     screen.blit(snake, (snake_x, snake_y))
     screen.blit(apple, apple_pos)
+
     pygame.display.update()
     # updating screen
     screen.fill(color_black)
