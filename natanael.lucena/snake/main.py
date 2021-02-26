72213f30168fbadb6a11c79dd50e33286b7d9a91
n4tm <nldm.eng20@uea.edu.br>
2021-02-26 12:32:24 -0400

    snake is now moving continuously

--- natanael.lucena/snake/main.py
+++ natanael.lucena/snake/main.py
@@ -34,12 +34,17 @@ def set_obj_coordinates(obj, x, y):
     obj.x = x
     obj.y = y
 
-
+previous_key = pygame.K_p
 # Check player key press
-def check_player_key(b):
-    global snake_direction
-    if event.key == pygame.K_w or event.key == pygame.K_s or event.key == pygame.K_a or event.key == pygame.K_d:
-        snake_direction[event.key] = b
+def check_player_key():
+    global snake_direction, previous_key
+    if (event.key == pygame.K_w and previous_key != pygame.K_s) or\
+            (event.key == pygame.K_s and previous_key != pygame.K_w) or\
+            (event.key == pygame.K_a and previous_key != pygame.K_d) or \
+            (event.key == pygame.K_d and previous_key != pygame.K_a):
+        snake_direction[previous_key] = False
+        snake_direction[event.key] = True
+        previous_key = event.key
 
 
 # Check key events in-game
@@ -48,9 +53,7 @@ def event_conditional():
     if event.type == pygame.QUIT:
         game_loop = False
     elif event.type == pygame.KEYDOWN:
-        check_player_key(True)
-    elif event.type == pygame.KEYUP:
-        check_player_key(False)
+        check_player_key()
 
 
 score_font = font(36)
@@ -63,17 +66,28 @@ score_font = font(36)
 
 # Check if the snake collided and the game is over
 def game_over():
-    if snake.y < 0 or snake.y > 720 or snake.x < 0 or snake. x > 1280:
+    if snake.y < 0:
+        set_obj_coordinates(snake, snake.x, 0)
         return True
+    if snake.y > 720 - snake.height:
+        set_obj_coordinates(snake, snake.x, 720 - snake.height)
+        return True
+    if snake.x < 0:
+        set_obj_coordinates(snake, 0, snake.y)
+        return True
+    if snake.x > 1280 - snake.width:
+        set_obj_coordinates(snake, 1280 - snake.width, snake.y)
+        return True
+    return False
 
 
 # Snake
 snake_img = img("snake")
 snake = snake_img.get_rect()
-move_keys = [pygame.K_w, pygame.K_d, pygame.K_s, pygame.K_a]
+move_keys = [pygame.K_w, pygame.K_d, pygame.K_s, pygame.K_a, pygame.K_p]
 snake_direction = {k: False for k in move_keys}
 snake_score = 0
-snake_vel = 10
+snake_vel = 1
 set_obj_coordinates(snake, center[0], center[1])
 
 
@@ -97,14 +111,6 @@ while game_loop:
                     snake.x += sign * snake_vel
                 else:
                     snake.y += sign * snake_vel
-        # if snake_direction[move_keys[0]]:
-        #    snake.y -= snake_vel
-        # if snake_direction[move_keys[1]]:
-        #    snake.x += snake_vel
-        # if snake_direction[move_keys[2]]:
-        #    snake.y += snake_vel
-        # if snake_direction[move_keys[3]]:
-        #    snake.x -= snake_vel
         screen.fill(COLOR_DARK_GREY)
         screen.blit(snake_img, snake)
         screen.blit(apple_img, apple)
@@ -112,6 +118,6 @@ while game_loop:
 
     # Update screen
     pygame.display.flip()
-    game_clock.tick(60)
+
 
 pygame.quit()
