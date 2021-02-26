cbd2d33ba2d794da69fe5d711355f16c32163677
Ronald Boadana <rcb.eng20@uea.edu.br>
2021-02-26 09:52:28 -0400

    defeat condition added

--- ronald.boadana/snake/snake_ronald-boadana.py
+++ ronald.boadana/snake/snake_ronald-boadana.py
@@ -5,9 +5,11 @@ import random
 
 pygame.init()
 
+# set the scores
 max_score = 10
 player_score = 0
 
+# set the colors and screen
 color_black = (0, 0, 0)
 color_white = (255, 255, 255)
 screen_size = (600, 600)
@@ -21,7 +23,7 @@ snake_pos = (snake_x, snake_y)
 
 # drawing apple
 apple = pygame.image.load('../snake/assets/ronald.boadana_apple.png')
-apple_pos = (random.randint(0, 590)//10 * 10, random.randint(0, 590)//10 * 10)
+apple_pos = (random.randint(0, 590) // 10 * 10, random.randint(0, 590) // 10 * 10)
 
 # score text
 score_font = pygame.font.Font('../snake/assets/PressStart2P.ttf', 30)
@@ -30,10 +32,10 @@ score_text_rect = score_text.get_rect()
 score_text_rect.center = (300, 25)
 
 # defeat text
-defeat_font = pygame.font.Font('../snake/assets/PressStart2P.ttf', 60)
+defeat_font = pygame.font.Font('../snake/assets/PressStart2P.ttf', 30)
 defeat_text = defeat_font.render('GAME OVER', True, color_white, color_black)
 defeat_text_rect = score_text.get_rect()
-defeat_text_rect.center = (350, 350)
+defeat_text_rect.center = (175, 275)
 
 # conditions to snake can move
 direction_x, direction_y = 10, 10
@@ -49,31 +51,34 @@ while game_on:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_w:
                 snake_y -= direction_y
+                snake = pygame.image.load('../snake/assets/ronald.boadana_snakehead_up.png')
             if event.key == pygame.K_s:
                 snake_y += direction_y
+                snake = pygame.image.load('../snake/assets/ronald.boadana_snakehead.png')
             if event.key == pygame.K_a:
                 snake_x -= direction_x
+                snake = pygame.image.load('../snake/assets/ronald.boadana_snakehead-left.png')
             if event.key == pygame.K_d:
                 snake_x += direction_x
+                snake = pygame.image.load('../snake/assets/ronald.boadana_snakehead-right.png')
+        print(snake_x, snake_y)
 
-    # victory condition
-    if player_score < max_score:
-        if snake_x == apple_pos:
-            player_score += 1
-            apple_pos = (random.randint(0, 590)//10*10, random.randint(0, 590)//10*10)
-            print(apple_pos)
-            print(snake_x)
-            print(snake_y)
+        # victory condition
+        if player_score < max_score:
+            if snake_x == apple_pos:
+                player_score += 1
+                apple_pos = (random.randint(0, 590) // 1010, random.randint(0, 590) // 1010)
 
-        score_text = score_font.render(str(player_score), True, color_white, color_black)
-
-    else:
-        if (snake_x == 0 or snake_y == 0) and (snake_x == 600 or snake_y == 600):
-            screen.blit(defeat_text, defeat_text_rect)
+            score_text = score_font.render(str(player_score), True, color_white, color_black)
 
     screen.blit(score_text, score_text_rect)
     screen.blit(snake, (snake_x, snake_y))
     screen.blit(apple, apple_pos)
+
+    # condition for the player to lose the game
+    if (snake_x == 0) or (snake_y == 40) or (snake_x == 570) or (snake_y == 570):
+        screen.fill(color_black)
+        screen.blit(defeat_text, defeat_text_rect)
 
     pygame.display.update()
     # updating screen
