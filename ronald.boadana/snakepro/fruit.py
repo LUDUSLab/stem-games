1bfd255c2e159d45bae5304aac958dcb155ca08f
Ronald Boadana <rcb.eng20@uea.edu.br>
2021-03-05 11:51:09 -0400

    some changes on fruit code

--- ronald.boadana/snakepro/fruit.py
+++ ronald.boadana/snakepro/fruit.py
@@ -1,7 +1,6 @@
 import pygame
 
-apple = pygame.image.load('../snake/assets/ronald.boadana_apple.png')
-apple = pygame.transform.scale(apple, [20, 20])
+apple = pygame.image.load('../snakepro/assets/ronald.boadana_apple.png')
 apple_pos = ((random.randint(60, 560) // 10 * 10), (random.randint(60, 560) // 10 * 10))
 
 
@@ -9,3 +8,23 @@ def apple_randomness_movement():
     apple_x = (random.randint(60, 560) // 10 * 10)
     apple_y = (random.randint(60, 560) // 10 * 10)
     return apple_x, apple_y
+
+
+grape = pygame.image.load('../snakepro/assets/ronald.boadana_grape.png.')
+grape_pos = ((random.randint(60, 560) // 10 * 10), (random.randint(60, 560) // 10 * 10))
+
+
+def grape_randomness_movement():
+    grape_x = (random.randint(60, 560) // 10 * 10)
+    grape_y = (random.randint(60, 560) // 10 * 10)
+    return grape_x, grape_y
+
+
+strawberry = pygame.image.load('../snakepro/assets/ronald.boadana_strawberry.png.')
+strawberry_pos = ((random.randint(60, 560) // 10 * 10), (random.randint(60, 560) // 10 * 10))
+
+
+def strawberry_randomness_movement():
+    strawberry_x = (random.randint(60, 560) // 10 * 10)
+    strawberry_y = (random.randint(60, 560) // 10 * 10)
+    return strawberry_x, strawberry_y
