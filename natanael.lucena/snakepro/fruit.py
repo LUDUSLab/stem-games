92433c42a1354e4983ae008fabe70343827c1a86
n4tm <nldm.eng20@uea.edu.br>
2021-03-08 12:06:57 -0400

    fix fruit random spawn location

--- natanael.lucena/snakepro/fruit.py
+++ natanael.lucena/snakepro/fruit.py
@@ -12,11 +12,12 @@ for i in range(len(fruits_imgs)):
 
 general_fruit = fruits_imgs[0][0].get_rect()
 
+
 def random_fruit(body_pos):
     global general_fruit  # Fruit rectangle coordinates
     while True:
         general_fruit.x = randrange(1, (window[0] // snake.w) - 1) * snake.w
-        general_fruit.y = randrange(1, (window[1] // snake.h) -1 ) * snake.h
+        general_fruit.y = randrange(4, (window[1] // snake.h) - 1) * snake.h
         if not any(pos == (general_fruit.x, general_fruit.y) for pos in
                    body_pos):  # Checks if the fruit spawn position is not the same as the snake's body
             break
--- natanael.lucena/snakepro/wall.py
+++ natanael.lucena/snakepro/wall.py
@@ -2,6 +2,7 @@ from config import img, window, screen
 
 wall_imgs = []
 
+
 def create_wall_img():
     for i in range(3):
         wall_imgs.insert(i, img("stone-block" + str(i+1)))
