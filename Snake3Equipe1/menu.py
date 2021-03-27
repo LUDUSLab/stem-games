e465b61b888047a975f5aa3849b6830ce4749af1
gabi.breval <gbdos.eng20@uea.edu.br>
2021-03-26 20:31:41 -0400

    Addinf settings

--- Snake3Equipe1/menu.py
+++ Snake3Equipe1/menu.py
@@ -13,6 +13,7 @@ class MainMenu:
 
         self._menu.add_button('Play', self._play_game)
         self._menu.add_button('Credits', self._credits)
+        self._menu.add_button('Settings', self._setting)
 
         self._menu.add_vertical_margin(100)
         self._menu.add_button('Quit', pygame_menu.events.EXIT)
@@ -23,6 +24,10 @@ class MainMenu:
 
     def _credits(self):
         CreditsMenu(self._height, self._width, theme=self._theme)
+        pygame.display.set_mode((self._width, self._height))
+
+    def _setting(self):
+        SettingMenu(self._height, self._width, theme=self._theme)
         pygame.display.set_mode((self._width, self._height))
 
 
@@ -36,6 +41,17 @@ class CreditsMenu:
         self._menu.add_label("Arthur")
         self._menu.add_label("Emanuel")
         self._menu.add_label("Gabriela Breval de Oliveira Santiago ")
+
+        self._menu.add_vertical_margin(100)
+        self._menu.add_button("Back", self._menu.disable)
+        self._menu.mainloop(self._screen)
+
+class SettingMenu:
+    def __init__(self, height, width, title="Credits", theme=pygame_menu.themes.THEME_DARK):
+        self._width, self._height = width, height
+        self._screen = pygame.display.set_mode((width, height))
+        self._menu = pygame_menu.Menu(height, width, title, theme=theme)
+
 
         self._menu.add_vertical_margin(100)
         self._menu.add_button("Back", self._menu.disable)
