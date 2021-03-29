a6b20210ff34738cc5bb5ac122205b72d7ee45a3
Ronald Boadana <rcb.eng20@uea.edu.br>
2021-03-29 09:57:03 -0400

    improvements on code, i'll adapt to other codes

--- Snake3Equipe2/wall.py
+++ Snake3Equipe2/wall.py
@@ -1,27 +1,30 @@
-from game_test_snake import *
+import pygame
+
+pygame.init()
+
+color_black = (255, 255, 255)
+window_size = (1280, 720)
+screen = pygame.display.set_mode(window_size)
+
+pygame.display.set_caption("screen test")
 
 
-class Wall(object):
-    def __init__(self, size: tuple = (40, 40), color: tuple = (200, 200, 200)):
-        self.size = size
-        self.color = color
-        self.wall = pygame.Surface(size)
+class Wall:
+    def __init__(self):
+        self.size = (40, 40)
+        self.color = (200, 200, 200)
+        self._wall = pygame.Surface(self.size)
+        self._wall.fill(self.color)
 
-    def draw_wall_borders(self, wall):
-        for i in range(120, 1280, 40):
-            self.screen.blit(self.wall, (i, 0))
-            self.screen.blit(self.wall, (i, 1240))
+    def draw_wall(self):
+        screen.blit(self._wall, (0, 32))
+        screen.blit(self._wall, (0, 608))
+        screen.blit(self._wall, (1248, 32))
+        screen.blit(self._wall, (1248, 608))
+        for i in range(32, 1248, 32):
+            screen.blit(self._wall, (i, 32))
+            screen.blit(self._wall, (i, 608))
 
-        for i in range(40, 720, 40):
-            self.screen.blit(self.wall, (0, i))
-            self.screen.blit(self.wall, (680, i))
-
-
-class Obstacles(Wall):
-    def __init__(self, size, color, wall):
-        super().__init__(size, color)
-        self.obstacle_1 = self.wall
-        self.obstacle_2 = self.wall
-
-    def draw_obstacles(self):
-        screen.blit(self.obstacle_1, (120, 120))
+        for i in range(64, 608, 32):
+            screen.blit(self._wall, (0, i))
+            screen.blit(self._wall, (1248, i))
