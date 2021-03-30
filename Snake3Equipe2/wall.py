fc2b4193d18d1fe4aebdebee526ec9e03afdf9a6
Ronald Boadana <rcb.eng20@uea.edu.br>
2021-03-30 14:25:42 -0400

    bug fixed: the upper wall was on top of arena's area/and some changes on obstacles coordinates

--- Snake3Equipe2/wall.py
+++ Snake3Equipe2/wall.py
@@ -9,7 +9,7 @@ class Wall(object):
     def draw_wall(self, surface):
         # for 'y' coordinate
         for i in range(120, 1200, 40):
-            pygame.draw.rect(surface, self.color, (i, 120) + self.size)
+            pygame.draw.rect(surface, self.color, (i, 80) + self.size)
             pygame.draw.rect(surface, self.color, (i, 600) + self.size)
 
         # for 'x' coordinate
@@ -25,16 +25,16 @@ class Obstacles(Wall):
 
     def draw_obstacles(self, surface):
         # 1
-        pygame.draw.rect(surface, self.color, (200, 240) + self.size)
-        pygame.draw.rect(surface, self.color, (240, 240) + self.size)
+        pygame.draw.rect(surface, self.color, (200, 200) + self.size)
         pygame.draw.rect(surface, self.color, (240, 200) + self.size)
+        pygame.draw.rect(surface, self.color, (240, 160) + self.size)
         # 2
+        pygame.draw.rect(surface, self.color, (360, 160) + self.size)
         pygame.draw.rect(surface, self.color, (360, 200) + self.size)
-        pygame.draw.rect(surface, self.color, (360, 240) + self.size)
         # 3
-        pygame.draw.rect(surface, self.color, (200, 360) + self.size)
-        pygame.draw.rect(surface, self.color, (240, 360) + self.size)
-        pygame.draw.rect(surface, self.color, (280, 360) + self.size)
+        pygame.draw.rect(surface, self.color, (240, 400) + self.size)
+        pygame.draw.rect(surface, self.color, (280, 400) + self.size)
+        pygame.draw.rect(surface, self.color, (320, 400) + self.size)
         # 4
         pygame.draw.rect(surface, self.color, (200, 480) + self.size)
         pygame.draw.rect(surface, self.color, (240, 480) + self.size)
@@ -43,11 +43,11 @@ class Obstacles(Wall):
         pygame.draw.rect(surface, self.color, (360, 480) + self.size)
         pygame.draw.rect(surface, self.color, (360, 520) + self.size)
         # 6
-        pygame.draw.rect(surface, self.color, (480, 280) + self.size)
-        pygame.draw.rect(surface, self.color, (520, 280) + self.size)
-        pygame.draw.rect(surface, self.color, (560, 240) + self.size)
-        pygame.draw.rect(surface, self.color, (560, 280) + self.size)
+        pygame.draw.rect(surface, self.color, (480, 240) + self.size)
+        pygame.draw.rect(surface, self.color, (520, 240) + self.size)
         pygame.draw.rect(surface, self.color, (560, 200) + self.size)
+        pygame.draw.rect(surface, self.color, (560, 240) + self.size)
+        pygame.draw.rect(surface, self.color, (560, 160) + self.size)
         # 7
         pygame.draw.rect(surface, self.color, (480, 440) + self.size)
         pygame.draw.rect(surface, self.color, (520, 440) + self.size)
@@ -55,11 +55,11 @@ class Obstacles(Wall):
         pygame.draw.rect(surface, self.color, (560, 480) + self.size)
         pygame.draw.rect(surface, self.color, (560, 520) + self.size)
         # 8
-        pygame.draw.rect(surface, self.color, (760, 280) + self.size)
-        pygame.draw.rect(surface, self.color, (800, 280) + self.size)
-        pygame.draw.rect(surface, self.color, (720, 240) + self.size)
-        pygame.draw.rect(surface, self.color, (720, 280) + self.size)
+        pygame.draw.rect(surface, self.color, (760, 240) + self.size)
+        pygame.draw.rect(surface, self.color, (800, 240) + self.size)
         pygame.draw.rect(surface, self.color, (720, 200) + self.size)
+        pygame.draw.rect(surface, self.color, (720, 240) + self.size)
+        pygame.draw.rect(surface, self.color, (720, 160) + self.size)
         # 9
         pygame.draw.rect(surface, self.color, (760, 440) + self.size)
         pygame.draw.rect(surface, self.color, (800, 440) + self.size)
@@ -67,22 +67,28 @@ class Obstacles(Wall):
         pygame.draw.rect(surface, self.color, (720, 480) + self.size)
         pygame.draw.rect(surface, self.color, (720, 520) + self.size)
         # 10
+        pygame.draw.rect(surface, self.color, (920, 160) + self.size)
         pygame.draw.rect(surface, self.color, (920, 200) + self.size)
-        pygame.draw.rect(surface, self.color, (920, 240) + self.size)
         # 11
         pygame.draw.rect(surface, self.color, (920, 480) + self.size)
         pygame.draw.rect(surface, self.color, (920, 520) + self.size)
         # 12
-        pygame.draw.rect(surface, self.color, (1080, 240) + self.size)
-        pygame.draw.rect(surface, self.color, (1040, 240) + self.size)
+        pygame.draw.rect(surface, self.color, (1080, 200) + self.size)
         pygame.draw.rect(surface, self.color, (1040, 200) + self.size)
+        pygame.draw.rect(surface, self.color, (1040, 160) + self.size)
         # 13
         pygame.draw.rect(surface, self.color, (1080, 480) + self.size)
         pygame.draw.rect(surface, self.color, (1040, 480) + self.size)
         pygame.draw.rect(surface, self.color, (1040, 520) + self.size)
         # 14
-        pygame.draw.rect(surface, self.color, (1080, 360) + self.size)
-        pygame.draw.rect(surface, self.color, (1040, 360) + self.size)
-        pygame.draw.rect(surface, self.color, (1000, 360) + self.size)
+        pygame.draw.rect(surface, self.color, (1040, 400) + self.size)
+        pygame.draw.rect(surface, self.color, (1000, 400) + self.size)
+        pygame.draw.rect(surface, self.color, (960, 400) + self.size)
         # 15
-        pygame.draw.rect(surface, self.color, (640, 360) + self.size)
+        pygame.draw.rect(surface, self.color, (240, 280) + self.size)
+        pygame.draw.rect(surface, self.color, (280, 280) + self.size)
+        pygame.draw.rect(surface, self.color, (320, 280) + self.size)
+        # 16
+        pygame.draw.rect(surface, self.color, (1040, 280) + self.size)
+        pygame.draw.rect(surface, self.color, (1000, 280) + self.size)
+        pygame.draw.rect(surface, self.color, (960, 280) + self.size)
