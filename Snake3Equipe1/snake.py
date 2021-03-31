bf77b61a041e39ff10daf44c3a570a6bd0f537da
Arthur Gustavo Paiva Carvalho <agpc.eng20@uea.edu.br>
2021-03-31 12:43:11 -0400

    sprites added.

--- Snake3Equipe1/snake.py
+++ Snake3Equipe1/snake.py
@@ -1,13 +1,20 @@
 class Snake:
 
-    def __init__(self, color_1, color_2, position):
+    def __init__(self, color_1, color_2, position, image):
         self._body1 = pygame.Surface((32, 32))
         self._body1.fill(color_1)
         self._body2 = pygame.Surface((32, 32))
         self._body2.fill(color_2)
+
+        self._head = pygame.image.load(image)
+
         self._position = position
         self._move_x = 0
         self._move_y = 0
+
+    @property
+    def position(self):
+        return self._position
 
     def new_body(self):
         self._position.append((1280, 640))
@@ -28,22 +35,22 @@ class Snake:
 
 class Player(Snake):
 
-    LEFT, UP, RIGHT, DOWN = 37, 38, 39, 40
+    W, A, S, D = 0, 1, 2, 3
 
     def snake_moves(self, direction):
-        if direction == self.LEFT:
+        if direction == self.S:
             self._move_x = -32
             self._move_y = 0
 
-        if direction == self.UP:
+        if direction == self.W:
             self._move_x = 0
             self._move_y = -32
 
-        if direction == self.RIGHT:
+        if direction == self.D:
             self._move_x = 32
             self._move_y = 0
 
-        if direction == self.DOWN:
+        if direction == self.S:
             self._move_x = 0
             self._move_y = 32
 
