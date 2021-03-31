bc120cd20306293ba2875365684605d0f034821a
Emanuel Henrique Oliveira Dias <ehod.eng20@uea.edu.br>
2021-03-31 15:55:20 -0400

    Fruits added

---
+++ Snake3Equipe1/fruits.py
@@ -0,0 +1,30 @@
+import pygame
+import random
+
+class Fruit:
+    def __init__(self):
+        self._pos = (640, 360)
+        self._fruits = ["assets/apple_sprite.png","assets/orange_sprite.png","assets/banana_sprite.png",
+                        "assets/grape_sprite.png"]
+        self._fruit = ("assets/apple_sprite.png")
+
+    @property
+    def position(self):
+        return self._pos
+
+    def changep(self):
+        self._pos = (random.randint(32, 1216), random.randint(64, 656))
+
+    def draw(self):
+        screen.blit(self._fruit, self._pos)
+
+    def choose(self):
+        vari = random.randint(0,20)
+        if vari in [1,2,3,4,5,6,7,8,9,10]:
+            self._fruit = ("assets/apple_sprite.png")
+        elif vari in [11,12,13,14,15]:
+            self._fruit = ("assets/orange_sprite.png")
+        elif vari in [16,17,18,19]:
+            self._fruit = ("assets/banana_sprite.png")
+        elif vari in [20]:
+            self._fruit = ("assets/grape_sprite.png")
