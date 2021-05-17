62f358ab14f8d6005297e9adcb8695c81d182f8d
n4tm <nldm.eng20@uea.edu.br>
2021-05-17 15:07:03 -0400

    ship movement working properly

--- AsteroidsEquipe2/ship.py
+++ AsteroidsEquipe2/ship.py
@@ -5,7 +5,7 @@ import gameobject
 class Ship(gameobject.GameObject):
     UP = pygame.Vector2(0, -1)
     MANEUVERABILITY = 3
-    ACCELERATION = 0.25
+    ACCELERATION = 0.10
 
     def __init__(self):
         self.sprite_path = "./assets/ship.png"
@@ -15,6 +15,7 @@ class Ship(gameobject.GameObject):
         self.rotated_sprite = pygame.image.load(self.sprite_path).convert_alpha()
         self.radius = self.sprite.get_height()/2
         self.img_rect = self.sprite.get_rect(center=(config.window.size[0]/2, config.window.size[1]/2))
+        self.accelerating = False
 
     def rotate(self, clockwise=True):
         sign = 1 if clockwise else -1
@@ -23,6 +24,7 @@ class Ship(gameobject.GameObject):
         self.rotated_sprite = pygame.transform.rotozoom(self.sprite, angle, 1.0)
 
     def accelerate(self):
+
         self.velocity += self.direction * self.ACCELERATION
 
     def check_ship_keys(self, key_pressed):
@@ -32,10 +34,23 @@ class Ship(gameobject.GameObject):
             self.rotate(clockwise=False)
         if key_pressed[pygame.K_SPACE] or key_pressed[pygame.K_x] or key_pressed[pygame.K_UP] or \
                 key_pressed[pygame.K_w]:
+            self.accelerating = True
             self.accelerate()
+        else:
+            self.accelerating = False
 
     def display(self):
         self.move()
+        if not self.accelerating:
+            if self.velocity[1] < 0:
+                self.velocity += pygame.Vector2(0, 0.008)
+            elif self.velocity[1] > 0:
+                self.velocity -= pygame.Vector2(0, 0.008)
+            if self.velocity[0] < 0:
+                self.velocity += pygame.Vector2(0.01, 0.008)
+            elif self.velocity[0] > 0:
+                self.velocity -= pygame.Vector2(0.01, 0.008)
+
         angle = self.direction.angle_to(self.UP)
         rotated_surface = pygame.transform.rotozoom(self.sprite, angle, 1.0)
         rotated_surface_size = pygame.Vector2(rotated_surface.get_size())
