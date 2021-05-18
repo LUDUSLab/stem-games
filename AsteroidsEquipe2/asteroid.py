f1342d0e630e1fc1a0b013014df642d23487e7b4
n4tm <nldm.eng20@uea.edu.br>
2021-05-18 16:31:38 -0400

    add asteroid score

--- AsteroidsEquipe2/asteroid.py
+++ AsteroidsEquipe2/asteroid.py
@@ -4,7 +4,6 @@ import gameobject
 
 
 class Asteroid(gameobject.GameObject):
-    score = 20
     min_asteroid_spawn_distance = 250
 
     def __init__(self, position, asteroid_callback, size=3):
@@ -17,10 +16,15 @@ class Asteroid(gameobject.GameObject):
         self.sprite = pygame.transform.rotozoom(pygame.image.load(self.sprite_path).convert_alpha(), 0, self.scale)
         self.velx = uniform(-1.5, 1.5)
         self.vely = uniform(-1.5, 1.5)
+        self.score = 20
         super().__init__(position, self.sprite, pygame.Vector2(self.velx, self.vely))
 
     def split(self):
         if self.size > 1:
             for _ in range(2):
                 asteroid = Asteroid(self.position, self.asteroid_callback, self.size-1)
+                if asteroid.size == 2:
+                    asteroid.score = 50
+                elif asteroid.size == 1:
+                    asteroid.score = 100
                 self.asteroid_callback(asteroid)
